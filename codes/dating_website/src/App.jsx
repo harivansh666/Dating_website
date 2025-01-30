db0cf3b24b2009567c224dc4dde75a32a1e71c03
Harivansh <jazzz1815@gmail.com>
2025-01-30 18:00:47 +0530

    install  talwind css

--- codes/dating_website/src/App.jsx
+++ codes/dating_website/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import react from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h3> vansh sharma </h3>
+      <div className="p-5  ">
+        <h1 className="text-red-500">This is Red Text</h1>
+        <h2 className="text-blue-600">This is Blue Text</h2>
+        <p className="text-green-400">This is Green Text</p>
+      </div>
+
     </>
   )
 }
