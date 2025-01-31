b9f0757fea73aa68ce55c45dea5caec6352727ec
Harman sahota <sahotaharman59111@gmail.com>
2025-01-31 14:28:02 +0530

    by hari

---
+++ codes/dating_website/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='w-full bg-amber-300'>
+      <div className='bg-green-200 h-10 w-10'>
+        <img 
+        className='border rounded-full w-full h-full '
+        src="https://images.unsplash.com/photo-1618042164219-62c820f10723?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dW5zcGFsc2h8ZW58MHx8MHx8fDA%3D" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Nav
