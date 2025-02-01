d11f00cfb39e4e58920f94a1272b4d218762ccda
Harivansh <jazzz1815@gmail.com>
2025-02-01 09:59:18 +0530

    djsfb

--- codes/dating_website/src/App.jsx
+++ codes/dating_website/src/App.jsx
@@ -2,6 +2,7 @@ import react from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <h2 className="text-blue-600">This is Blue Text</h2>
         <p className="text-green-400">This is Green Text</p>
       </div>
+      <Nav />
 
     </>
   )
