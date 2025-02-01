bfc2ccd0087cd48adeb725648d2db434463f0500
Harman sahota <sahotaharman59111@gmail.com>
2025-02-01 06:56:28 +0530

    added feature navbar

--- codes/dating_website/src/App.jsx
+++ codes/dating_website/src/App.jsx
@@ -2,17 +2,13 @@ import react from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <div className="p-5  ">
-        <h1 className="text-red-500">This is Red Text</h1>
-        <h2 className="text-blue-600">This is Blue Text</h2>
-        <p className="text-green-400">This is Green Text</p>
-      </div>
-
+     <Navbar />
     </>
   )
 }
--- codes/dating_website/src/components/Navbar.jsx
+++ codes/dating_website/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { people } from '../data/People'
+import { IoMdNotifications } from "react-icons/io";
+import { AiFillMessage } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+     <div style={{backgroundColor: "#d61856"}} className='w-full flex justify-evenly  gap-2 p-5'>
+      <h1 className='font-bold text-4xl'>Logo</h1>
+    
+      <div className='rounded-full flex gap-3 ml-20 mt-4'>
+      {people.map((e,i)=>{
+        return <img key={i}
+        className='h-25 w-25  rounded-full object-cover cursor-pointer'
+        src={e.Image}alt="" />
+       })}
+      </div>
+
+       <div className='mt-12 '>
+       <button className='bg-pink-500 px-7 py-1  rounded  font-bold text-start text-white cursor-pointer'>Dating Tips <br /> 
+       <span className='text-3xl text-black'>Using AI</span></button>
+       </div>
+        
+        <div className='flex gap-4'>
+        <IoMdNotifications className='text-white text-4xl  cursor-pointer'/>
+        <AiFillMessage className="text-white     text-4xl  cursor-pointer "    />
+        </div>
+
+     </div>
+     
   )
 }
 
