37139ec33b86f423827befc54da7897cda29c498
Harivansh Sharma <jazzz1815@gmail.com>
2025-03-29 23:44:38 +0530

    Update app.js

--- codes/backend/app.js
+++ codes/backend/app.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: 'https://vibelydating.onrender.com',
+    origin: 'https://vibelydating.netlify.app/',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
      allowedHeaders: ['Content-Type', 'Authorization']
