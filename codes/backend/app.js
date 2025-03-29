6f94bf4f10596d2583e0153bc9f770d4db8a7bb1
Harivansh Sharma <jazzz1815@gmail.com>
2025-03-29 21:44:00 +0530

    Update app.js

--- codes/backend/app.js
+++ codes/backend/app.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ['http://localhost:5173', 'https://vibely-skix.onrender.com']
     credentials: true,
   })
 );
